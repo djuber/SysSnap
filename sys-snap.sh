4275054c064c59653ae62e73091a2d262280ec7c
echoe <sfelshman@gmail.com>
2014-02-03 20:11:18 -0600

    Updating with where my sys-snap is so far.
    i've been editing this for our specific setup and have been changing the logfiles that the script makes in order to make it simpler to quickly parse the logs. so think of this first commit as a sort of 'everything that's happened in that space' at once.

--- sys-snap.sh
+++ sys-snap.sh
@@ -2,7 +2,7 @@
 ########################################################################
 # SysSnap is a very simple system monitoring script.
 ########################################################################
-#    Copyright (C) 2012
+#    Copyright (C) 2014
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -30,18 +30,17 @@ SLEEP_TIME="1m"
 # You *MUST* put a slash at the end.
 ROOT_DIR="~/"
 
-# Sometimes you won't have mysql and/or you won't have the root password to put in a .my.cnf file
-# if that's the case, set this to "" (0 evals true in bash)
-MYSQL=1
-# If the server has lighttpd or some other webserver, set this to ""
-APACHE=1
-# If the server has cpanel set this to 1
-CPANEL=1
 # If you want extended data, set this to 1
+# With this enabled it takes approximately 10x more space. [50k/minute vs. 500k/minute.]
 MAX_DATA=""
 
+# If you want to change the delimiter you can do that here.
+#This is temporarily disabled and possibly will be removed so that I can use other delimiters per process.
+#With these separate delimiters you can run something like:
+# sed -n '/mysqllog/,/vmstatlog/p' $logfile
+#This will give you the mysql log [to a point].
 ################################################################################
-#  If you don't know what your doing, don't change anything below this line
+#  If you don't know what you're doing, don't change anything below this line
 ################################################################################
 
 #######################
@@ -67,22 +66,22 @@ T2=$(echo $ROOT_DIR | sed -e $T1)
 ROOT_DIR=$T2
 
 if [ ! -d ${ROOT_DIR} ] ; then
-        echo $ROOT_DIR is not a directory
-        exit 1
+  echo $ROOT_DIR is not a directory
+  exit 1
 fi
 
 if [ ! -w ${ROOT_DIR} ] ; then
-        echo $ROOT_DIR is not writable
-        exit 1
+  echo $ROOT_DIR is not writable
+  exit 1
 fi
 
 # if a system-snapshot directory exists, save the data and empty it.
 # if it does't, create it.  
 if [ -d ${ROOT_DIR}system-snapshot ]; then
-        tar -czf ${ROOT_DIR}system-snapshot.${date}.${hour}${min}.tar.gz ${ROOT_DIR}system-snapshot
-        rm -fr ${ROOT_DIR}system-snapshot/*
+  tar -czf ${ROOT_DIR}system-snapshot.${date}.${hour}${min}.tar.gz ${ROOT_DIR}system-snapshot
+  rm -fr ${ROOT_DIR}system-snapshot/*
 else
-	mkdir ${ROOT_DIR}system-snapshot
+  mkdir ${ROOT_DIR}system-snapshot
 fi
 
 ################
@@ -90,51 +89,75 @@ fi
 ################
 
 for ((;;)) ; do
-        # update time
-        date=`date`
-        hour=`date +%H`
-        min=`date +%M`
+  # update time
+  date=`date`
+  hour=`date +%H`
+  min=`date +%M`
 
-        # go to the next log file
-        mkdir -p ${ROOT_DIR}system-snapshot/$hour
-        current_interval=$hour/$min
+  # go to the next log file
+  mkdir -p ${ROOT_DIR}system-snapshot/$hour
+  current_interval=$hour/$min
 
 	LOG=${ROOT_DIR}system-snapshot/$current_interval.log
 	
-        # clear the log if it already exists
-        [ -e $LOG ] && rm $LOG
+  # Clear the log if it already exists
+  [ -e $LOG ] && rm $LOG
 
-        # ### start actually logging ### #
+  # ### Start actually logging ### #
 
-        # basic stuff
-        load=`cat /proc/loadavg` #least cpu
-        echo "$date $hour $min --> load: $load" >> $LOG
-        cat /proc/meminfo >> $LOG
-        vmstat 1 10 >> $LOG
-        ps auwwxf >> $LOG
-        netstat -anp >> $LOG
+  # Top output! It's simple!
+  echo "toplog" >>$LOG
+  top -c -n1 -b >>$LOG
+  # We always have mysql on customer servers so we don't need this optional, and it is easy to read.
+  echo "mysqllog" >>$LOG
+  mysqladmin proc >>$LOG
+  # Some vmstat information.
+  echo "vmstatlog" >>$LOG
+  vmstat 1 10 >>$LOG
+	# Memory information.
+  echo "meminfolog" >>$LOG
+  cat /proc/meminfo >>$LOG
+	# Ps aux, for data!
+  echo "psauwwxflog" >>$LOG
+  ps auwwxf >> $LOG
+	# Expansive netstat.
+  echo "netstatlog" >>$LOG
+  netstat -anp >> $LOG
+  # diskspace
+  echo "diskspacelog" >>$LOG
+  df -h >> $LOG
 
-        # optional logging
-        if [ $MYSQL ]; then
-                mysqladmin proc  >> $LOG
-        fi
-        if [ $CPANEL ] ; then
-                lynx --dump localhost/whm-server-status  >> $LOG
-        else if [ $APACHE ]; then
-                lynx -width=1024 -dump http://localhost/server-status|egrep '(Client.+Request|GET|POST|HEAD)' >> $LOG
-             fi
-        fi
-        if [ $MAX_DATA ]; then
-                for i in `ps aux | grep nobody | awk '{print $2}'` ; do ls -al /proc/$i | grep cwd | grep home; done >> $LOG
-                lsof >> $LOG  #a lot more data, useful to track down random processes
-        fi
+  # Automatic optional logs
 
-        # rotate the "current" pointer
-        rm -rf ${ROOT_DIR}system-snapshot/current
-        ln -s $LOG ${ROOT_DIR}system-snapshot/current
+  # This gets apache or litespeed status. The check checks for litespeed using litespeed's suggested way, and protects against finding the grep by isolating it.
+  # LiteSpeed  logs are per http://www.litespeedtech.com/support/forum/showthread.php?t=6150.
+  if [[ -z `ps -ef | grep '[l]itespeed'` ]]; then
+    echo "apachelog" >>$LOG
+    lynx --dump localhost/whm-server-status >>$LOG
+  else
+    echo "litespeedlog" >>$LOG
+    cat /tmp/lshttpd/.rtreport* >>$LOG
+  fi
+  # This grabs privvmpages, but only if /user_beancounters exists
+  if [ -a /proc/user_beancounters ]; then
+    echo "privvmpageslog" >>$LOG
+    cat /proc/user_beancounters | head -n2 | tail -n1 >>$LOG
+    grep privvmpages /proc/user_beancounters >>$LOG
+  fi
 
-        sleep $SLEEP_TIME
+  # Optional: change top settings to enable these
+  # Max Data [per original syssnap]
+  if [ $MAX_DATA ]; then
+    echo "max_datalog" >>$LOG
+    for i in `ps aux | grep nobody | awk '{print $2}'` ; do ls -al /proc/$i | grep cwd | grep home; done >>$LOG
+    lsof >>$LOG
+  fi
+
+  # Rotate the "current" pointer
+  rm -rf ${ROOT_DIR}system-snapshot/current
+  ln -s $LOG ${ROOT_DIR}system-snapshot/current
+
+  sleep $SLEEP_TIME
 
 done
 #EOF
-
